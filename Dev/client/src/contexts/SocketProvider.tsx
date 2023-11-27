ff6bd459e2d7da2f151774eddd9cf44716a6969e
Connor Carroll <ccarroll@sjrollins.com>
2023-11-27 15:16:21 -0500

    oy vey

--- Dev/client/src/contexts/SocketProvider.tsx
+++ Dev/client/src/contexts/SocketProvider.tsx
@@ -14,7 +14,7 @@ export const useSocket = () => {
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
   console.log("Connection from socket provider started...");
-  const socket = io(`http://${config.domainName}:8000`);
+  const socket = io(`https://${config.domainName}`);
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
